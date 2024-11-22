c5f2678fd30d03e9b03ed33b2457523e9881a76e
AndreSantiagoDominguez <233393@ids.upchiapas.edu.mx>
2024-11-22 13:44:48 -0600

    resolviendo conflictos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,10 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { HomePComponent } from './home/home-p/home-p.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home', component: HomePComponent},
+  {path: 'signIn', component: LoginComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
