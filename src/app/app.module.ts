cf4e6b4fad06a7116f444319dd5e3fdc38c07f6f
Alexis Guzman <spartanthelexvro@gmail.com>
2024-11-22 13:42:37 -0600

    Protegiendo vistas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HomeModule } from './home/home.module';
 
 @NgModule({
   declarations: [AppComponent, LoginComponent],
-  imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule ],
+  imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule, HomeModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/home/home-p/home-p.component.ts
+++ src/app/home/home-p/home-p.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UsersService } from '../../service/users.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home-p',
   templateUrl: './home-p.component.html',
-  styleUrl: './home-p.component.css'
+  styleUrl: './home-p.component.css',
 })
-export class HomePComponent {
+export class HomePComponent implements OnInit {
+  constructor(private _user: UsersService, private router: Router) {}
 
+  ngOnInit(): void {
+    const numberUsers = this._user.validateUser();
+
+    if (numberUsers == 0) {
+      this.router.navigate(['/signIn']);
+    }
+  }
 }
